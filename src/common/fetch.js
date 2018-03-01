df4a2793d49a5dd5b4e557b00631572b4d4c8d1f
gcc <gavin_008@163.com>
2018-03-01 17:35:32 +0800

    init

---
+++ src/common/fetch.js
@@ -0,0 +1,38 @@
+import wepy from 'wepy'
+
+const api = '/api'
+
+export const common = function (type, url, data, options = {}) {
+	const config = {
+		method: type,
+		url: `${api}${url}`,
+		data: data,
+		dataType: json,
+		headers: {
+			'content-type': 'application/json;charset=utf-8',
+		},
+	}
+
+	return wepy.request(config).then(res => {
+		if (res.success) {
+			return res.data
+		} else {
+			res.error
+		}
+	})
+}
+
+export const get = function (url, data, options) {
+	return common('GET', url, data, options)
+}
+
+export const post = function (url, data, options) {
+	return common('POST', url, data, options)
+}
+
+const install = function (instance) {
+	instance.$get = get
+	instance.$post = post
+}
+
+export default install
