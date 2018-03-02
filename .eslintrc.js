fb9b751d154c52a57b335a6c9d04debbee058dec
gcc <gavin_008@163.com>
2018-03-02 17:12:00 +0800

    更改了.eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,24 @@ module.exports = {
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'space-before-function-paren': 0
+    'space-before-function-paren': 0,
+    "comma-dangle": [
+      "error",
+      {
+        "arrays": "always-multiline",
+        "objects": "always-multiline",
+        "imports": "always-multiline",
+        "exports": "always-multiline",
+        "functions": "always-multiline",
+      }
+    ],
+    "indent": [
+      "error",
+      "tab",
+      {
+        "SwitchCase": 1
+      }
+    ],
+    "no-tabs": [0]
   }
 }
